fd69137fd9987ba17df02bc1b4fbfe921fca397a
Nikos Argalias <nik.argalias@gmail.com>
2022-01-08 18:46:50 +0200

    create CarScreen (Page) component

---
+++ src/screens/CarScreen/CarScreen.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledCarScreen = styled.div`
+    & > :not(:last-child) {
+        margin-bottom: var(--gt-lg);
+    }
+`;
+
+export default StyledCarScreen;
---
+++ src/screens/CarScreen/CarScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledCarScreen from './CarScreen.styled';
+
+const CarScreen = () => {
+    return (
+        <StyledCarScreen>
+        </StyledCarScreen>
+    );
+};
+
+export default CarScreen;
