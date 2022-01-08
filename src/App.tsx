bb64b86fc5d851928aa6a28818862e3d3913718d
Nikos Argalias <nik.argalias@gmail.com>
2022-01-08 18:51:15 +0200

    add CarContextProvider and CarScreen in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import CarContextProvider from './context/cars/carsContext';
+import CarScreen from './screens/CarScreen/CarScreen';
+import './styles/main.scss';
 
 function App() {
-    return <div className='App'></div>;
+    return (
+        <div className='App'>
+            <CarContextProvider>
+                <CarScreen />
+            </CarContextProvider>
+        </div>
+    );
 }
 
 export default App;
