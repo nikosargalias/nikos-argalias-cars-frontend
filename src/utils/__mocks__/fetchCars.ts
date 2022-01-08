dcec956d182730e6b0458baac89d3205a01a4d07
Nikos Argalias <nik.argalias@gmail.com>
2022-01-08 14:39:30 +0200

    write mock for fetchCars

---
+++ src/utils/__mocks__/fetchCars.ts
@@ -0,0 +1,22 @@
+export const fetchCars = (searchQuery?: string) => {
+    return [
+        {
+            objectId: '1',
+            Year: 2020,
+            Make: 'Audi',
+            Model: 'A3',
+            Category: 'Hatchback',
+            createdAt: '0',
+            updatedAt: '0',
+        },
+        {
+            objectId: '2',
+            Year: 2020,
+            Make: 'Mercedes',
+            Model: 'A200',
+            Category: 'Hatchback',
+            createdAt: '0',
+            updatedAt: '0',
+        },
+    ];
+};
