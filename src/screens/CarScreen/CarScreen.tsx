b5cc70fefd3550ccdb3420f784abe10c93297134
Nikos Argalias <nik.argalias@gmail.com>
2022-01-08 19:06:26 +0200

    add CarTable to CarScreen

--- src/screens/CarScreen/CarScreen.tsx
+++ src/screens/CarScreen/CarScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CarTable from '../../components/CarTable/CarTable';
 import StyledCarScreen from './CarScreen.styled';
 
 const CarScreen = () => {
     return (
         <StyledCarScreen>
+            <CarTable />
         </StyledCarScreen>
     );
 };
