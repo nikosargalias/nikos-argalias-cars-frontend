400dd782d8d167bcb4fc361e345319ecceb7dcaf
Nikos Argalias <nik.argalias@gmail.com>
2022-01-08 20:39:26 +0200

    render PhoneticCarWors component

--- src/screens/CarScreen/CarScreen.tsx
+++ src/screens/CarScreen/CarScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CarTable from '../../components/CarTable/CarTable';
 import CarForm from '../../components/Forms/CarForm/CarForm';
+import PhoneticCarWords from '../../components/PhoneticCarWords/PhoneticCarWords';
 import StyledCarScreen from './CarScreen.styled';
 
 const CarScreen = () => {
     return (
         <StyledCarScreen>
+            <PhoneticCarWords />
             <CarForm />
             <CarTable />
         </StyledCarScreen>
