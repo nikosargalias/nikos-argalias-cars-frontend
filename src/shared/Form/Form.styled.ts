63dcb094231a421be385667c4ed87e0a282e31f8
Nikos Argalias <nik.argalias@gmail.com>
2022-01-13 10:22:23 +0200

    Improve shared form styles

--- src/shared/Form/Form.styled.ts
+++ src/shared/Form/Form.styled.ts
@@ -5,10 +5,22 @@ const StyledForm = styled.form`
     flex-direction: column;
 
     & > :not(:last-child) {
-        margin-bottom: var(--gt-sm);
+        margin-bottom: var(--gt-md);
     }
 
     & .grp {
+        display: flex;
+        justify-content: space-between;
+
+        label {
+            flex-basis: 30%;
+            min-width: max-content;
+        }
+        input {
+            flex-basis: 70%;
+            padding: 0.2rem;
+        }
+
         & > :first-child {
             margin-right: var(--gt-sm);
         }
